86108d299f7bf19451c31ea409a9c7b9b8bf9969
Sumon Ahmed <bdsumon4u@gmail.com>
2023-01-14 18:48:55 +0600

    Featured Categories: Xx4

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -39,14 +39,14 @@
                     @if (count($featured_categories) > 0)
                         <ul class="list-unstyled mb-0 row gutters-5">
                             @foreach ($featured_categories as $key => $category)
-                                <li class="minw-0 col-4 col-md mt-3">
+                                <li class="minw-0 col-3 col-md mt-3">
                                     <a href="{{ route('products.category', $category->slug) }}" class="d-block rounded bg-white p-2 text-reset shadow-sm">
                                         <img
                                             src="{{ static_asset('assets/img/placeholder.jpg') }}"
                                             data-src="{{ uploaded_asset($category->banner) }}"
                                             alt="{{ $category->getTranslation('name') }}"
                                             class="lazyload img-fit"
-                                            height="78"
+                                            height="64"
                                             onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder-rect.jpg') }}';"
                                         >
                                         <div class="text-truncate fs-12 fw-600 mt-2 opacity-70">{{ $category->getTranslation('name') }}</div>
