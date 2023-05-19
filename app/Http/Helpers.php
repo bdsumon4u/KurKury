13e19d4dd7b2bea8bcfbd3793c5e024ff2f5150e
Sumon Ahmed <bdsumon4u@gmail.com>
2023-05-19 13:55:44 +0600

    Simple Price

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -184,6 +184,7 @@ if (!function_exists('currency_symbol')) {
 if (!function_exists('format_price')) {
     function format_price($price, $isMinimize = false)
     {
+        return $price;
         if (get_setting('decimal_separator') == 1) {
             $fomated_price = number_format($price, get_setting('no_of_decimals'));
         } else {
