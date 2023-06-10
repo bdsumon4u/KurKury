b9b219ce07f8706f2b5ec3060b5198a564a81036
Sumon Ahmed <bdsumon4u@gmail.com>
2023-06-11 02:56:14 +0600

    wip

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -373,10 +373,12 @@ class OrderController extends Controller
         if (($qty = request('quantity')) && ($dtl = request('detail'))) {
             $orderDetail = $order->orderDetails->where('id', $dtl)->first();
             // stock er kono kaj korini
+            $added = $qty - $orderDetail->quantity;
             $orderDetail->quantity = $qty;
             $orderDetail->save();
             $order->unsetRelation('orderDetails');
             $order->load('orderDetails');
+            $order->grand_total = $order->grand_total + ($orderDetail->price + $orderDetail->tax) * $added;
         }
         $order_shipping_address = json_decode($order->shipping_address);
         $delivery_boys = User::where('city', $order_shipping_address->city)
